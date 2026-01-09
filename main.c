c86d549f3e556b08b476b992da1272c830877d5f
Lukasz Niebrzydowski <lukasz.niebrzydowski.113367@student.pb.edu.pl>
2026-01-09 14:25:21 +0100

    Dodanie mozliwosci wyszukiwania posta po Nazwie autoria lub ilosci reportow. Dodane zostalo rowniez sortowanie. Nie do konca jasnym bylo dla mnie kryterium, które mowilo o sortowaniu wedlug wybranego pola - dlatego zrobilem to w taki sposob, aby to uzytkownik wybieral kolumne po ktorej odbedzie sie sortowanie. Jesli chodzi o sortowanie kolumn tekstowych jedyna sensowna kolumna wydawala sie byc kolumna z nazwa uzytkownika, poniewaz sortowanie po kolumnie tresci czy statusie wydaje sie byc bez sensu. Natomiast jesli chodzi o sortowanie po kolumnach liczbowych pozostaly kolumny ID oraz ILOSC ZGLOSZEN, wiec dalem mozliwosc wyboru.

--- main.c
+++ main.c
@@ -116,12 +116,12 @@ quick_talk_post_reported *read_text_file(const char *sciezka)
 void show_list(quick_talk_post_reported *header)
 {
     quick_talk_post_reported *current = header;
-    printf("\n%-5s | %-15s | %-25s | %-20s | %s\n", "ID", "AUTOR", "POWOD", "STATUS", "TRESC");
-    printf("----------------------------------------------------------------------------------------------------------------------\n");
+    printf("\n%-5s | %-15s | %-25s | %-15s | %-20s | %s\n", "ID", "AUTOR", "POWOD", "ILOSC ZGLOSZEN", "STATUS", "TRESC");
+    printf("----------------------------------------------------------------------------------------------------------------------");
 
     while (current)
     {
-        printf("\n%-5d | %-15s | %-25s | %-20s | %.40s...\n", current->post_id, current->post_creator_id, get_reason_str(current->post_report_reason), get_status_str(current->post_mod_status), current->post_description);
+        printf("\n%-5d | %-15s | %-25s | %-11d zgl.| %-20s | %.40s...\n", current->post_id, current->post_creator_id, get_reason_str(current->post_report_reason), current->post_report_counter, get_status_str(current->post_mod_status), current->post_description);
         current = current->next;
     }
 }
@@ -159,7 +159,13 @@ void add_post(quick_talk_post_reported **header)
     scanf(" %280[^\n]", new->post_description);
     printf("Wybierz kategorie (0-Spam, 1-Hejt, 2-Wulgaryzmy, 3-Fejk-news, 4-Nieodpowiednie): ");
     int reason_choice;
-    scanf("%d", &reason_choice);
+    if (scanf("%d", &reason_choice) != 1)
+    {
+        printf("Kategoria spoza zakresu!\n");
+        while (getchar() != '\n')
+            ;
+        return;
+    }
     new->post_report_reason = (enum post_report_reason)reason_choice;
 
     new->post_report_counter = 1;
@@ -282,6 +288,183 @@ void edit_post_status(quick_talk_post_reported *header)
     }
 }
 
+void search_by_author(quick_talk_post_reported *header)
+{
+    if (header == NULL)
+    {
+        printf("Baza jest pusta!\n");
+        return;
+    }
+
+    char search_term[101];
+    printf("Podaj nazwe autora lub jej poczatek: ");
+    scanf(" %100[^\n]", search_term);
+    int len = strlen(search_term);
+    int found_count = 0;
+
+    printf("\nWyniki wyszukiwania dla: \"%s\"\n", search_term);
+    printf("\n%-5s | %-15s | %-25s | %-15s | %-20s | %s\n", "ID", "AUTOR", "POWOD", "ILOSC ZGLOSZEN", "STATUS", "TRESC");
+    printf("----------------------------------------------------------------------------------------------------------------------");
+
+    quick_talk_post_reported *current = header;
+    while (current != NULL)
+    {
+        if (strncmp(current->post_creator_id, search_term, len) == 0)
+        {
+            printf("\n%-5d | %-15s | %-25s | %-11d zgl.| %-20s | %.40s...\n",
+                   current->post_id, current->post_creator_id, get_reason_str(current->post_report_reason), current->post_report_counter, get_status_str(current->post_mod_status), current->post_description);
+
+            found_count++;
+        }
+        current = current->next;
+    }
+    if (found_count == 0)
+    {
+        printf("Nie znaleziono zadnych postow pasujacych do kryteriow. \n");
+    }
+    else
+    {
+        printf("\nZnaleziono %d post(ow).\n", found_count);
+    }
+}
+
+void search_by_reports(quick_talk_post_reported *header)
+{
+    if (header == NULL)
+    {
+        printf("Baza jest pusta!\n");
+        return;
+    }
+
+    int min_reports;
+    printf("Wyswietl posty, ktorych ilosc zgloszen wynosi przynajmniej: ");
+    if (scanf("%d", &min_reports) != 1)
+    {
+        printf("Bledna wartosc!\n");
+        while (getchar() != '\n')
+            ;
+        return;
+    }
+
+    int found_count = 0;
+    printf("\nPosty z liczba zgloszen wieksza lub rowna %d:\n", min_reports);
+    printf("\n%-5s | %-15s | %-25s | %-15s | %-20s | %s\n", "ID", "AUTOR", "POWOD", "ILOSC ZGLOSZEN", "STATUS", "TRESC");
+    printf("----------------------------------------------------------------------------------------------------------------------");
+
+    quick_talk_post_reported *current = header;
+    while (current != NULL)
+    {
+        if (current->post_report_counter >= min_reports)
+        {
+            printf("\n%-5d | %-15s | %-25s | %-11d zgl.| %-20s | %.40s...\n",
+                   current->post_id, current->post_creator_id, get_reason_str(current->post_report_reason), current->post_report_counter, get_status_str(current->post_mod_status), current->post_description);
+
+            found_count++;
+        }
+        current = current->next;
+    }
+    if (found_count == 0)
+    {
+        printf("Brak postow spelniajacych to kryterium.\n");
+    }
+}
+
+void swap_data(quick_talk_post_reported *a, quick_talk_post_reported *b)
+{
+    int temp_id = a->post_id;
+    a->post_id = b->post_id;
+    b->post_id = temp_id;
+
+    int temp_count = a->post_report_counter;
+    a->post_report_counter = b->post_report_counter;
+    b->post_report_counter = temp_count;
+
+    enum post_report_reason temp_reason = a->post_report_reason;
+    a->post_report_reason = b->post_report_reason;
+    b->post_report_reason = temp_reason;
+
+    enum post_mod_status temp_status = a->post_mod_status;
+    a->post_mod_status = b->post_mod_status;
+    b->post_mod_status = temp_status;
+
+    char temp_text[281];
+    strcpy(temp_text, a->post_creator_id);
+    strcpy(a->post_creator_id, b->post_creator_id);
+    strcpy(b->post_creator_id, temp_text);
+
+    strcpy(temp_text, a->post_description);
+    strcpy(a->post_description, b->post_description);
+    strcpy(b->post_description, temp_text);
+}
+
+void sort_by_author(quick_talk_post_reported *header)
+{
+    if (header == NULL)
+    {
+        return;
+    }
+    int swapped;
+    quick_talk_post_reported *ptr1;
+    quick_talk_post_reported *lptr = NULL;
+
+    do
+    {
+        swapped = 0;
+        ptr1 = header;
+
+        while (ptr1->next != lptr)
+        {
+            if (strcmp(ptr1->post_creator_id, ptr1->next->post_creator_id) > 0)
+            {
+                swap_data(ptr1, ptr1->next);
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+    printf("Lista zostala posortowana alfabetycznie po autorach.\n");
+}
+
+void sort_by_numeric(quick_talk_post_reported *header, int mode)
+{
+    if (header == NULL)
+    {
+        return;
+    }
+    int swapped;
+    quick_talk_post_reported *ptr1;
+    quick_talk_post_reported *lptr = NULL;
+
+    do
+    {
+        swapped = 0;
+        ptr1 = header;
+
+        while (ptr1->next != lptr)
+        {
+            int condition = 0;
+            if (mode == 1)
+            {
+                condition = (ptr1->post_id > ptr1->next->post_id);
+            }
+            else if (mode == 2)
+            {
+                condition = (ptr1->post_report_counter > ptr1->next->post_report_counter);
+            }
+
+            if (condition)
+            {
+                swap_data(ptr1, ptr1->next);
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+    printf("Lista zostala posortowana liczbowo.\n");
+}
+
 int main()
 {
     quick_talk_post_reported *list = read_text_file(INPUT_FILE);
@@ -294,6 +477,8 @@ int main()
         printf("2. Dodaj nowy post\n");
         printf("3. Usun post\n");
         printf("4. Edytuj status posta\n");
+        printf("5. Wyszukaj posty\n");
+        printf("6. Sortowanie\n");
         printf("0. Wyjdz i zapisz\n");
         printf("Wybor: ");
         scanf("%d", &choice);
@@ -314,6 +499,54 @@ int main()
             break;
         case 4:
             edit_post_status(list);
+            break;
+        case 5:
+            printf("\nWybierz kryterium:\n1. Szukaj autora (tekstowe)\n2. Szukaj po liczbie zgloszen (liczbowe)\nWybor: ");
+            int search_choice;
+            if (scanf("%d", &search_choice) != 1)
+            {
+                printf("Bledna wartosc!\n");
+                while (getchar() != '\n')
+                    ;
+            }
+            if (search_choice == 1)
+            {
+                search_by_author(list);
+            }
+            else if (search_choice == 2)
+            {
+                search_by_reports(list);
+            }
+            break;
+        case 6:
+            printf("\nPo jakiej kolumnie sortowac?\n");
+            printf("1. Autor\n");
+            printf("2. ID\n");
+            printf("3. Liczba zgloszen\n");
+            printf("Wybor: ");
+            int s_choice;
+            if (scanf("%d", &s_choice) != 1)
+            {
+                printf("Bledna wartosc!\n");
+                while (getchar() != '\n')
+                    ;
+            }
+            if (s_choice == 1)
+            {
+                sort_by_author(list);
+            }
+            else if (s_choice == 2)
+            {
+                sort_by_numeric(list, 1);
+            }
+            else if (s_choice == 3)
+            {
+                sort_by_numeric(list, 2);
+            }
+            else
+            {
+                printf("Niepoprawny wybor.\n");
+            }
             break;
         default:
             printf("Bledny wybor!\n");
